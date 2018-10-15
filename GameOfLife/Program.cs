30aae3d9bfbac551742be6e3dc7849fa9e4cc38c
NivaLado <mark080897@gmail.com>
2018-10-15 16:43:14 +0300

    Second Commit

---
+++ GameOfLife/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            UserInput init = new UserInput();
+
+            Universe universe = new Universe(init.width, init.height);
+
+            Console.ReadKey();
+        }
+
+    }
+}
